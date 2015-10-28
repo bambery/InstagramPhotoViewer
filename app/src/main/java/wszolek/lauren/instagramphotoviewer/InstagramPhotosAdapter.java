cdc82ee82d11b838cecaea224c44dbcbc94a08e2
Lauren Wszolek <lwszolek@groupon.com>
2015-10-27 20:26:20 -0700

    added view holder because bewilderingly I forgot one....

--- app/src/main/java/wszolek/lauren/instagramphotoviewer/InstagramPhotosAdapter.java
+++ app/src/main/java/wszolek/lauren/instagramphotoviewer/InstagramPhotosAdapter.java
@@ -20,6 +20,20 @@ import java.util.List;
 public class InstagramPhotosAdapter extends ArrayAdapter<InstagramPhoto> {
     // what data do we need from the activity?
     // inputs: context and data source
+
+    private static class ViewHolder {
+        TextView username;
+        ImageView profilePicture;
+        ImageView clockIcon;
+        TextView createdAt;
+        ImageView likeIcon;
+        TextView likeCount;
+        ImageView instaPhoto;
+        TextView caption;
+        TextView commentCount;
+        TextView lastComment;
+        TextView secondToLastComment;
+    }
     public InstagramPhotosAdapter(Context context, List<InstagramPhoto> objects) {
         super(context, android.R.layout.simple_list_item_1, objects);
     }
@@ -31,13 +45,53 @@ public class InstagramPhotosAdapter extends ArrayAdapter<InstagramPhoto> {
         //get data item for this position
         InstagramPhoto photo = getItem(position);
         // check if we are using a recycled view, if not, need to inflate
+        ViewHolder viewHolder;
         if (convertView == null) {
+            viewHolder = new ViewHolder();
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_photo, parent, false);
+
+            // profile photo
+            viewHolder.profilePicture = (ImageView) convertView.findViewById(R.id.ivProfilePicture);
+
+            // username
+            viewHolder.username = (TextView) convertView.findViewById(R.id.tvUsername);
+
+            // clock icon
+            viewHolder.clockIcon = (ImageView) convertView.findViewById(R.id.ivClockIcon);
+
+            // time since posted
+            viewHolder.createdAt = (TextView) convertView.findViewById(R.id.tvCreatedAt);
+
+            // like count icon
+            viewHolder.likeIcon = (ImageView) convertView.findViewById(R.id.ivLikeIcon);
+
+            //like count
+            viewHolder.likeCount = (TextView) convertView.findViewById(R.id.tvLikeCount);
+
+            // the photo
+            viewHolder.instaPhoto = (ImageView) convertView.findViewById(R.id.ivPhoto);
+
+            // image caption
+            viewHolder.caption = (TextView) convertView.findViewById(R.id.tvCaption);
+
+            //comment count
+            viewHolder.commentCount = (TextView) convertView.findViewById(R.id.tvCommentCount);
+
+            //last comment
+            viewHolder.lastComment = (TextView) convertView.findViewById(R.id.tvLastComment);
+
+            //second to last comment
+            viewHolder.secondToLastComment = (TextView) convertView.findViewById(R.id.tvSecondToLastComment);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
+        // populate data
+
         //profile photo
-        ImageView ivProfilePicture = (ImageView) convertView.findViewById(R.id.ivProfilePicture);
-        ivProfilePicture.setImageResource(0);
+        viewHolder.profilePicture.setImageResource(0);
         // create transform to round the photo
         Transformation transformation = new RoundedTransformationBuilder()
                                                 .borderColor(R.color.pale_grey)
@@ -50,62 +104,52 @@ public class InstagramPhotosAdapter extends ArrayAdapter<InstagramPhoto> {
                 .load(photo.userProfilePictureUrl)
                 .fit()
                 .transform(transformation)
-                .into(ivProfilePicture);
+                .into(viewHolder.profilePicture);
 
         // username
-        TextView tvUsername = (TextView) convertView.findViewById(R.id.tvUsername);
-        tvUsername.setText(photo.username);
+        viewHolder.username.setText(photo.username);
 
         // clock icon and set the color
-        ImageView ivClockIcon = (ImageView) convertView.findViewById(R.id.ivClockIcon);
-        ivClockIcon.setImageResource(0);
-        ivClockIcon.setColorFilter(ContextCompat.getColor(getContext(), R.color.pale_grey));
-        ivClockIcon.setImageResource(R.drawable.ic_action_clock);
+        viewHolder.clockIcon.setImageResource(0);
+        viewHolder.clockIcon.setColorFilter(ContextCompat.getColor(getContext(), R.color.pale_grey));
+        viewHolder.clockIcon.setImageResource(R.drawable.ic_action_clock);
 
-        // time posted
-        TextView tvCreatedAt = (TextView) convertView.findViewById(R.id.tvCreatedAt);
-        tvCreatedAt.setText(getRelativeTime(photo.createdAt));
+        // time since posted
+        viewHolder.createdAt.setText(getRelativeTime(photo.createdAt));
 
         // like count icon & set color
-        ImageView ivLikeIcon = (ImageView) convertView.findViewById(R.id.ivLikeIcon);
-        ivLikeIcon.setImageResource(0);
-        ivLikeIcon.setColorFilter(ContextCompat.getColor(getContext(), R.color.dark_navy));
-        ivLikeIcon.setImageResource(R.drawable.ic_action_heart);
+        viewHolder.likeIcon.setImageResource(0);
+        viewHolder.likeIcon.setColorFilter(ContextCompat.getColor(getContext(), R.color.dark_navy));
+        viewHolder.likeIcon.setImageResource(R.drawable.ic_action_heart);
 
         // like count
-        TextView tvLikeCount = (TextView) convertView.findViewById(R.id.tvLikeCount);
-        String likeString = getLikesString(photo);
-        tvLikeCount.setText(likeString);
+        viewHolder.likeCount.setText(getLikesString(photo));
 
         // photo
-        ImageView ivPhoto = (ImageView) convertView.findViewById(R.id.ivPhoto);
+
         // clear out the image view (if the view is recycled for the photo)
-        ivPhoto.setImageResource(0);
+        viewHolder.instaPhoto.setImageResource(0);
         //insert the image using picasso
         Picasso.with(getContext())
                 .load(photo.imageUrl)
                 .placeholder(R.drawable.placeholder)
-                .into(ivPhoto);
+                .into(viewHolder.instaPhoto);
 
         //image caption
-        TextView tvCaption = (TextView) convertView.findViewById(R.id.tvCaption);
         String captionText = getCaptionText(photo.username, photo.caption);
-        tvCaption.setText(Html.fromHtml(captionText));
+        viewHolder.caption.setText(Html.fromHtml(captionText));
 
         // comment count
-        TextView tvCommentCount = (TextView) convertView.findViewById(R.id.tvCommentCount);
-        String commentCountText = getCommentCountText(photo.commentCount);
-        tvCommentCount.setText(commentCountText);
+        viewHolder.commentCount.setText(getCommentCountText(photo.commentCount));
 
         //last comment
-        TextView tvLastComment = (TextView) convertView.findViewById(R.id.tvLastComment);
         String lastCommentText = getCaptionText(photo.lastCommentUsername, photo.lastCommentText);
-        tvLastComment.setText(Html.fromHtml(lastCommentText));
+        viewHolder.lastComment.setText(Html.fromHtml(lastCommentText));
 
         //cheating - second to last comment
-        TextView tvSecondToLastComment = (TextView) convertView.findViewById(R.id.tvSecondToLastComment);
+
         String secondToLastCommentText = getCaptionText(photo.secondToLastCommentUsername, photo.secondToLastCommentText);
-        tvSecondToLastComment.setText(Html.fromHtml(secondToLastCommentText));
+        viewHolder.secondToLastComment.setText(Html.fromHtml(secondToLastCommentText));
 
         //return the created item as a view
         return convertView;
@@ -127,11 +171,7 @@ public class InstagramPhotosAdapter extends ArrayAdapter<InstagramPhoto> {
                        + "</b></font> "
                        + text;
     }
-//        if(likeCount == 0){
-//            return "";
-//        }
-//        return null;
-//    }
+
     private String getCommentCountText(int commentCount){
         return "View all " + Integer.toString(commentCount) + " comments";
     }
